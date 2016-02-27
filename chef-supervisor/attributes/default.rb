7cb7f04ae5e72512991d399b69f200dac13b59f2
Nikhilesh Vaishy <nikhilesh.limetray@gmail.com>
2016-02-27 17:14:39 +0530

    supervisor cookbook

---
+++ chef-supervisor/attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: supervisor
+# Attributes:: default
+#
+# Copyright 2012-2013, Escape Studios
+#
+
+default['supervisor']['programs'] = {}
+default['supervisor']['sockfile'] = "/var/run/supervisor.sock"
